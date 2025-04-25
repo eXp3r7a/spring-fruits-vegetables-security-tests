6e499438c6011e47d77dd3c21bac9f81f7755eec
Denis <denis.irhan@gmail.com>
2025-04-25 16:56:46 +0300

    Change extends from CrudRepo to JpaRepo in ProductRepository

--- src/main/java/com/example/spring_validation_fruits_vegetables/repositories/ProductRepository.java
+++ src/main/java/com/example/spring_validation_fruits_vegetables/repositories/ProductRepository.java
@@ -1,7 +1,8 @@
 package com.example.spring_validation_fruits_vegetables.repositories;
 
 import com.example.spring_validation_fruits_vegetables.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
